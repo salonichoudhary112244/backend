8e9d4f5faede1ef21be22b390bbcdc233869213e
saloni112244 <salonichoudhary112244@gmail.com>
2026-01-18 22:47:41 +0530

    iamge hover

--- cosmetic/src/components/product/ProductImageGallery.jsx
+++ cosmetic/src/components/product/ProductImageGallery.jsx
@@ -1,35 +1,49 @@
 import { useState, useEffect } from "react";
 
 export default function ProductImageGallery({ images }) {
-  // const [active, setActive] = useState(null);
-const [active, setActive] = useState(images?.[0]);
 
+  // only image ki
+// const [active, setActive] = useState(images?.[0]);
+
+// useEffect(() => {
+//     setActive(images?.[0]);
+//   }, [images]);
+
+const [active, setActive] = useState(null);
 
 useEffect(() => {
-    setActive(images?.[0]);
-  }, [images]);
+  if (!images || images.length === 0) return;
+
+  const primary =
+    images.find(img => img.isPrimary) || images[0];
+
+  setActive(primary);
+}, [images]);
 
 
   if (!images || images.length === 0) {
     return <img src="/no-image.png" alt="No Image" />;
   }
-  //   useEffect(() => {
-  //   if (images && images.length > 0) {
-  //     setActive(images[0]);   // 🔥 RESET ACTIVE IMAGE
-  //   }
-  // }, [images]);
 
   return (
     <div className="image-gallery">
       {/* Thumbnails */}
       <div className="thumbs">
         {images.map(img => (
+          // <img
+          //   key={img.id}
+          //   src={img.imageUrl}
+          //   className={active?.id === img.id ? "active" : ""}
+          //   onClick={() => setActive(img)}
+          // />
+
           <img
-            key={img.id}
-            src={img.imageUrl}
-            className={active?.id === img.id ? "active" : ""}
-            onClick={() => setActive(img)}
-          />
+  key={img.id}
+  src={img.imageUrl}
+  className={active?.id === img.id ? "active" : ""}
+  onMouseEnter={() => setActive(img)}   // 👈 hover change
+  onClick={() => setActive(img)}        // 👈 click change
+/>
         ))}
       </div>
 
